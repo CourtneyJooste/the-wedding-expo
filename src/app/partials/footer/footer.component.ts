7be0b3a62c4be4ded73e0629ac2932ce40a97dc9
Courtney Jooste <courtney@codelab.io>
2017-11-28 15:59:14 +0200

    feat: home page additions

--- src/app/partials/footer/footer.component.ts
+++ src/app/partials/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  this_year: number = (new Date()).getFullYear();
   constructor() { }
 
   ngOnInit() {
