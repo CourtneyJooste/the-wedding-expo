c9c68a5c9cab09fcc8173d9c0de19571a60ac8ae
Courtney Jooste <courtney@codelab.io>
2017-11-30 11:40:10 +0200

    feat: show activation page added and colour scheme reworked

--- src/app/components/show/show.component.ts
+++ src/app/components/show/show.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowComponent implements OnInit {
 
+  exhibitors: any[] = [
+    {name: "exhibitor name", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", image: "assets/img/workshops/ws1.jpg", url: "http://test.com" },
+    {name: "exhibitor name", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", image: "assets/img/workshops/ws1.jpg", url: "http://test.com" }   
+  ];
+
+  sponsors: any[] = [
+    {image: "assets/img/sponsors/1.jpg", title: "test", url: "http://test.com"},
+    {image: "assets/img/sponsors/1.jpg", title: "test", url: "http://test.com"},
+    {image: "assets/img/sponsors/1.jpg", title: "test", url: "http://test.com"},
+    {image: "assets/img/sponsors/1.jpg", title: "test", url: "http://test.com"},
+    {image: "assets/img/sponsors/1.jpg", title: "test", url: "http://test.com"},
+    {image: "assets/img/sponsors/1.jpg", title: "test", url: "http://test.com"},
+  ];
+
+  competitions: any[] = [
+    {name: "competition name", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", image: "assets/img/competitions/c1.jpg", url: "http://test.com" },
+    {name: "competition name", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor", image: "assets/img/competitions/c1.jpg", url: "http://test.com" }   
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
--- src/app/partials/navbar-sm/navbar-sm.component.ts
+++ src/app/partials/navbar-sm/navbar-sm.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-sm',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-sm.component.scss']
 })
 export class NavbarSmComponent implements OnInit {
+  @Input('sidenav') sidenav: any;
 
   constructor() { }
 
