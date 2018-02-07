0b4303083c81ed1f2fd760f5d227072eab74efc3
Courtney Jooste <courtney@codelab.io>
2018-02-07 13:44:14 +0200

    feat: changes - ticket link

--- src/app/partials/upcoming-expos/upcoming-expos.component.ts
+++ src/app/partials/upcoming-expos/upcoming-expos.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class UpcomingExposComponent implements OnInit {
 
   expos: any[] = [
-    {name: "Johannesburg", location: "Ticketpro Dome", dates: "10 & 11 march 2018", details: "Price: <b>R120.00</b> per person<br>R30 per child (2 to 10 years)", button: "coming soon"},
+    {name: "Johannesburg", location: "Ticketpro Dome", dates: "10 & 11 march 2018", details: "Price: <b>R120.00</b> per person<br>R30 per child (2 to 10 years)", button: "buy tickets", link: "https://weddingexpo.nutickets.co.za/March"},
 
-    {name: "Cape Town", location: "CTICC", dates: "9 & 10 JUNE 2018", details: "Price: <b>R100.00</b> per person<br>R30 per child (2 to 10 years)", button: "buy tickets - t.b.a."},
+    {name: "Cape Town", location: "CTICC", dates: "9 & 10 JUNE 2018", details: "Price: <b>R100.00</b> per person<br>R30 per child (2 to 10 years)", button: "buy tickets - t.b.a.", link: "#"},
 
-    {name: "Durban", location: "DEC", dates: "28 & 29 JULY 2018", details: "Price: <b>R100.00</b> per person<br>R30 per child (2 to 10 years)", button: "buy tickets - t.b.a."},
+    {name: "Durban", location: "DEC", dates: "28 & 29 JULY 2018", details: "Price: <b>R100.00</b> per person<br>R30 per child (2 to 10 years)", button: "buy tickets - t.b.a.", link: "#"},
 
-    {name: "Johannesburg", location: "Ticketpro Dome", dates: "25 & 26 AUG 2018", details: "Price: <b>R120.00</b> per person<br>R30 per child (2 to 10 years)", button: "buy tickets - t.b.a."},
+    {name: "Johannesburg", location: "Ticketpro Dome", dates: "25 & 26 AUG 2018", details: "Price: <b>R120.00</b> per person<br>R30 per child (2 to 10 years)", button: "buy tickets - t.b.a.", link: "#"},
   ];
 
   constructor() { }
